a233e352f7a0a24a74861cc2c2736d0ee0d726cd
argierincon <argierincon@gmail.com>
2022-09-28 16:38:57 -0500

    feature: adding elements in home component

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+
+    <template #main-content>
+      <h6>Holi</h6>
+    </template>
+
+    <template #nav-menu>
+      <Header />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/components/layouts/Default.vue";
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+    Layout,
+  },
+};
+</script>
