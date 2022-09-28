59bb3222749fa05696b4453f7e27ffcc71e8a4f6
argierincon <argierincon@gmail.com>
2022-09-28 16:37:54 -0500

    feature: adding layout default

---
+++ src/components/layouts/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <slot name="header"></slot>
+    <main>
+      <slot name="main-content"></slot>
+    </main>
+    <slot name="nav-menu"></slot>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
