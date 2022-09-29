bbb7d6a427af0059978ae2abc7c830ee0400e4b2
argierincon <argierincon@gmail.com>
2022-09-29 16:09:17 -0500

    refactor: adding props updating css transform

--- src/components/app/NavigationTabs.vue
+++ src/components/app/NavigationTabs.vue
@@ -1,34 +1,54 @@
 <template>
   <section class="navigation-tabs">
-    <button
-      class="btn"
-      v-for="tab in tabs"
-      :key="tab"
-      @click="() => switchTab(tab)"
-    >
-      {{ tab }}
-    </button>
-    <transition name="slide">
-      <component :is="currentTab"></component>
-    </transition>
+    <nav class="tabs">
+      <ul>
+        <li
+          v-for="(tab, idx) in tabs"
+          :key="tab"
+          :class="{ 'is-active': tab.isActive }"
+          @click="() => switchTab(tab, idx)"
+        >
+          {{ tab.label }}
+        </li>
+      </ul>
+    </nav>
+    <section class="tab-content">
+      <transition name="fade">
+        <component :is="currentTab"></component>
+      </transition>
+    </section>
   </section>
 </template>
 
 <script>
-import BalanceBox from "@/components/app/BalanceBox.vue";
-import TransactionBox from "@/components/app/TransactionBox.vue";
-
 export default {
-  components: { BalanceBox, TransactionBox },
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
   data() {
     return {
-      currentTab: "BalanceBox",
-      tabs: ["BalanceBox", "TransactionBox"],
+      currentTab: null,
+      // tabs: [
+      //   {
+      //     isActive: true,
+      //     label: "Total ahorrado",
+      //     component: "BalanceBox",
+      //   },
+      // ],
     };
+  },
+  mounted() {
+    this.currentTab = this.tabs[0].component;
   },
   methods: {
     switchTab(tab) {
-      return (this.currentTab = tab);
+      this.currentTab = tab.component;
+      this.tabs.find((tab) => tab.isActive).isActive = false;
+      tab.isActive = true;
+      console.log(tab, "TAB");
     },
   },
 };
@@ -42,14 +62,48 @@ export default {
   }
 }
 
-.slide-enter-from,
-.slide-leave-to {
-  transform: translateX(80px);
+.tabs {
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-grow: 1;
+    flex-shrink: 0;
+    border-bottom-color: #dbdbdb;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+
+    li {
+      padding: 0.5em 1em;
+      text-align: center;
+      font-size: 14px;
+      cursor: pointer;
+    }
+
+    li.is-active {
+      margin-bottom: -1px;
+      border-bottom-width: 1px;
+      border-bottom-style: solid;
+      border-bottom-color: $color-primary;
+      color: $color-primary;
+    }
+  }
+}
+
+.tab-content {
+  padding: 1rem 0;
+}
+
+// Clases de transicion
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(20px);
   opacity: 0;
 }
 
-.slide-enter-active,
-.slide-enter-to {
-  transition: all 0.3s ease-out;
+.fade-enter-active,
+.fade-enter-to {
+  transition: all 0.5s ease;
 }
 </style>
