8a3c3e8af87de6755faee3dae9bd3c82d3fab31f
argierincon <argierincon@gmail.com>
2022-09-29 09:08:01 -0500

    refacrtor: relocating components

---
+++ src/components/app/BalanceBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="balance-box">
+    <p class="balance-box__title">BALANCE</p>
+    <h6 class="balance-box__amount">s/3,500.40</h6>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.balance-box {
+  @include violet-box;
+
+  &__title {
+    font-weight: 500;
+  }
+
+  &__amount {
+    margin-top: 0.25rem;
+    font-weight: 600;
+  }
+}
+</style>
---
+++ src/components/app/TransactionBox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="transaction-box">
+    <p class="transaction-box__title">Ingresos</p>
+    <p class="transaction-box__amount">s/3,500.40</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.transaction-box {
+  @include violet-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+
+  &__title {
+    font-weight: 500;
+  }
+
+  &__amount {
+    font-weight: 400;
+  }
+}
+</style>
