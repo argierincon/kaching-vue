a5fb9a83dbfc7f02b075e2754ce5e8cde07750e1
argierincon <argierincon@gmail.com>
2022-09-30 11:26:27 -0500

    feature: adding home chart section

---
+++ src/components/app/SavingsChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h6>{{ amountCurrency }}</h6>
+  </section>
+</template>
+
+<script>
+import { currencyFormater } from "/utils/currencyFormater";
+
+export default {
+  data() {
+    return {
+      amount: null,
+      totalAmount: 1000,
+      currencyFormater,
+    };
+  },
+  computed: {
+    showAmount() {
+      return this.amount !== null ? this.amount : this.totalAmount;
+    },
+    amountCurrency() {
+      return currencyFormater.format(this.showAmount);
+    },
+  },
+};
+</script>
