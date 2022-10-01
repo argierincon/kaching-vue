32bbce5dd77a4c1431155f3b4fb7b802b4373cf3
argierincon <argierincon@gmail.com>
2022-09-30 19:59:49 -0500

    feature: adding menu item component

---
+++ src/components/app/navMenu/MenuItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu-item">
+    <svg-icon class="icon" type="mdi" :path="icon"></svg-icon>
+    <p class="label">{{ label }}</p>
+  </div>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiBell } from "@mdi/js";
+
+export default {
+  components: {
+    SvgIcon,
+  },
+  props: {
+    icon: { type: String, default: mdiBell },
+    label: { type: String, default: "HOME" },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  transition: all 0.4s ease;
+
+  &:hover {
+    .label,
+    .icon {
+      color: $color-primary;
+      transition: all 0.4s ease;
+    }
+  }
+}
+
+.icon {
+  color: $color-black-light;
+}
+
+.label {
+  font-size: 0.65rem;
+  font-weight: 300;
+  color: $color-black-light;
+}
+</style>
