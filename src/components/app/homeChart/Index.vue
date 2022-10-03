bb6d8a2c79f019f1c1f19f9ba420c030a6fdb156
argierincon <argierincon@gmail.com>
2022-10-03 18:34:09 -0500

    feature: adding dynamic label

--- src/components/app/homeChart/Index.vue
+++ src/components/app/homeChart/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="chart-section">
-    <p class="label-saved">{{ test }}</p>
+    <p class="label-saved">{{ graphicLabel }}</p>
     <h6 class="amount-saved">{{ currencyFormater.format(showAmount) }}</h6>
     <Graphic :amounts="listAmounts" @pointSelected="selected" />
   </section>
@@ -20,90 +20,90 @@ export default {
       movements: [
         {
           id: 1,
-          date: new Date("10-02-2022"),
+          date: new Date("09-26-2022"),
           transactionType: "outcome",
-          transactionName: "Almuerzo parrilla",
-          amount: -49.2,
+          transactionName: "Venezuela",
+          amount: -250.0,
         },
         {
           id: 2,
-          date: new Date("10-01-2022"),
-          transactionType: "outcome",
-          transactionName: "Almuerzo ramen",
-          amount: -120.0,
-        },
-        {
-          id: 3,
-          date: new Date("09-30-2022"),
-          transactionType: "income",
-          transactionName: "Pago quincena",
-          amount: 2697.64,
-        },
-        {
-          id: 4,
-          date: new Date("09-30-2022"),
-          transactionType: "outcome",
-          transactionName: "Almuerzo chaufa y makis",
-          amount: -44.7,
-        },
-        {
-          id: 5,
-          date: new Date("09-28-2022"),
-          transactionType: "outcome",
-          transactionName: "Mercado del mes Metro",
-          amount: -340.53,
-        },
-        {
-          id: 6,
-          date: new Date("09-28-2022"),
-          transactionType: "outcome",
-          transactionName: "Ahorro en otra cuenta",
-          amount: -508.54,
-        },
-        {
-          id: 7,
-          date: new Date("09-27-2022"),
-          transactionType: "outcome",
-          transactionName: "Retiro de efectivo",
-          amount: -50.0,
-        },
-        {
-          id: 8,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Serv. mantenimiento",
-          amount: -160.43,
-        },
-        {
-          id: 9,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Gas Calida equivocado",
-          amount: -7.2,
-        },
-        {
-          id: 10,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Enel",
-          amount: -31.0,
-        },
-        {
-          id: 11,
           date: new Date("09-26-2022"),
           transactionType: "outcome",
           transactionName: "Cena Rokys",
           amount: -54.7,
         },
         {
-          id: 11,
+          id: 3,
           date: new Date("09-26-2022"),
           transactionType: "outcome",
-          transactionName: "Venezuela",
-          amount: -250.0,
+          transactionName: "Enel",
+          amount: -31.0,
+        },
+        {
+          id: 4,
+          date: new Date("09-26-2022"),
+          transactionType: "outcome",
+          transactionName: "Gas Calida equivocado",
+          amount: -7.2,
+        },
+        {
+          id: 5,
+          date: new Date("09-26-2022"),
+          transactionType: "outcome",
+          transactionName: "Serv. mantenimiento",
+          amount: -160.43,
+        },
+        {
+          id: 6,
+          date: new Date("09-27-2022"),
+          transactionType: "outcome",
+          transactionName: "Retiro de efectivo",
+          amount: -50.0,
+        },
+        {
+          id: 7,
+          date: new Date("09-28-2022"),
+          transactionType: "outcome",
+          transactionName: "Ahorro en otra cuenta",
+          amount: -508.54,
+        },
+        {
+          id: 8,
+          date: new Date("09-28-2022"),
+          transactionType: "outcome",
+          transactionName: "Mercado del mes Metro",
+          amount: -340.53,
+        },
+        {
+          id: 9,
+          date: new Date("09-30-2022"),
+          transactionType: "outcome",
+          transactionName: "Almuerzo chaufa y makis",
+          amount: -44.7,
+        },
+        {
+          id: 10,
+          date: new Date("09-30-2022"),
+          transactionType: "income",
+          transactionName: "Pago quincena",
+          amount: 2697.64,
+        },
+        {
+          id: 11,
+          date: new Date("10-01-2022"),
+          transactionType: "outcome",
+          transactionName: "Almuerzo ramen",
+          amount: -120.0,
+        },
+        {
+          id: 12,
+          date: new Date("10-02-2022"),
+          transactionType: "outcome",
+          transactionName: "Almuerzo parrilla",
+          amount: -49.2,
         },
       ],
-      test: "TEST",
+      graphicLabel: "Disponible a la fecha",
     };
   },
   computed: {
@@ -114,20 +114,19 @@ export default {
       return currencyFormater.format(this.showAmount);
     },
     listAmounts() {
-      const last30Days = this.movements
-        .filter((m) => {
-          const today = new Date();
-          const lastDays = today.setDate(today.getDate() - 30);
+      const last30Days = this.movements.filter((m) => {
+        const today = new Date();
+        const lastDays = today.setDate(today.getDate() - 30);
 
-          return m.date > lastDays;
-        })
-        .map((m) => m.amount);
+        return m.date > lastDays;
+      });
 
       return last30Days.map((m, i) => {
         const lastMovements = last30Days.slice(0, i + 1);
 
         return lastMovements.reduce((suma, current) => {
-          return suma + current;
+          current.sum = suma + current.amount;
+          return suma + current.amount;
         }, 0);
       });
     },
@@ -142,11 +141,28 @@ export default {
   },
   methods: {
     selected(pointSelected) {
+      this.graphicLabel = this.formatingDate(pointSelected);
       this.amount = pointSelected;
 
       if (pointSelected === undefined) {
         this.amount = this.listAmounts[0];
       }
+    },
+    formatingDate(pointSelected) {
+      const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+
+      if (pointSelected === undefined) {
+        return this.movements[0].date.toLocaleDateString("es-PE", options);
+      }
+
+      return this.movements
+        .find((ele) => ele.sum === pointSelected)
+        .date.toLocaleDateString("es-PE", options);
     },
   },
 };
