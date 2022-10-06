068127630a928c8430bc528fedd537723d7994d2
argierincon <argierincon@gmail.com>
2022-10-06 15:08:07 -0500

    feature: adding input component

---
+++ src/components/inputs/Input.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}</label>
+    <input
+      class="input"
+      v-model="vmodel"
+      :type="type"
+      :placeholder="placeholder"
+      :disabled="disabled"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, default: "text" },
+    vmodel: { type: String, default: "" },
+    label: { type: String, default: "Label" },
+    placeholder: { type: String, default: "Placeholder" },
+    disabled: { type: String, default: false },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.field {
+  @include field-styles;
+}
+
+.label {
+  @include label-field;
+}
+
+.input {
+  @include standard-inputs;
+  @include inset-shadow-inputs;
+  width: 100%;
+
+  &:hover {
+    @include inputs-hover;
+  }
+
+  &:active,
+  &:focus {
+    @include inputs-active-focus;
+  }
+
+  &:disabled {
+    @include inputs-disabled;
+  }
+}
+
+.field.is-floating-in-label,
+.field.is-floating-label {
+  position: relative;
+}
+
+.field.is-floating-label .label {
+  top: -0.775em;
+  padding-left: 0.125em;
+  padding-right: 0.125em;
+
+  position: absolute;
+  left: 1em;
+  font-size: 0.75rem;
+  // background-color: transparent;
+  z-index: 5;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  max-width: calc(100% - 2em);
+  overflow: hidden;
+}
+
+.field.is-floating-label .label:before {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0.775em;
+  left: 0;
+  right: 0;
+  height: 0.375em;
+  background-color: #fff;
+  z-index: -1;
+}
+</style>
