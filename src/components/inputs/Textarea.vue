314cad9ab38b884ddee05382d48e8827730de000
argierincon <argierincon@gmail.com>
2022-10-06 15:08:27 -0500

    feature: adding textarea component

---
+++ src/components/inputs/Textarea.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}</label>
+    <textarea
+      class="textarea"
+      name=""
+      id=""
+      rows="4"
+      :placeholder="placeholder"
+      :disabled="disabled"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, default: "text" },
+    vmodel: { type: String, default: "" },
+    label: { type: String, default: "Label" },
+    placeholder: { type: String, default: "Placeholder" },
+    disabled: { type: String, default: false },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.field {
+  @include field-styles;
+}
+
+.label {
+  @include label-field;
+}
+
+.textarea {
+  @include standard-inputs;
+  @include inset-shadow-inputs;
+  width: 100%;
+
+  &:hover {
+    @include inputs-hover;
+  }
+
+  &:active,
+  &:focus {
+    @include inputs-active-focus;
+  }
+
+  &:disabled {
+    @include inputs-disabled;
+  }
+}
+</style>
