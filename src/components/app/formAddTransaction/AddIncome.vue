c2619d4caf35cb93e378a3e6e0d2ec88a5fb4dc7
argierincon <argierincon@gmail.com>
2022-10-06 10:31:29 -0500

    feature: adding form modal

---
+++ src/components/app/formAddTransaction/AddIncome.vue
@@ -0,0 +1,43 @@
+<template>
+  <form action="" class="form-add-transaction">
+    <p>Registrar Ingreso</p>
+    <select name="tipo">
+      <option value="">Transferencia</option>
+      <option value="">Efectivo</option>
+    </select>
+    <input placeholder="Titular" type="text" />
+    <input placeholder="Monto" type="text" />
+    <textarea
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="Descripcion"
+    ></textarea>
+
+    <div class="form-add-transaction__buttons">
+      <Btn label="Cancelar" btnType="secondary" />
+      <Btn label="Guardar" />
+    </div>
+  </form>
+</template>
+
+<script setup>
+import Btn from "../../public/Btn.vue";
+</script>
+
+<style lang="scss" scoped>
+.form-add-transaction {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  // align-items: center;
+  background: thistle;
+
+  &__buttons {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
