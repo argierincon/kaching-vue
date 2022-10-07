c5ca379c3be544f76e0950dc6232c2fa2b7f4440
argierincon <argierincon@gmail.com>
2022-10-07 10:42:06 -0500

    feature: adding custom select

--- src/components/inputs/Select.vue
+++ src/components/inputs/Select.vue
@@ -3,13 +3,15 @@
     <label class="label">{{ label }}</label>
     <div class="box-select">
       <select
-        class="select"
-        name="tipo"
-        v-model="vmodel"
+        :class="{ select: true, 'is-empty': isEmpty }"
+        v-model="localModel"
         :placeholder="placeholder"
         :disabled="disabled"
       >
-        <option class="option" value="">Transferencia</option>
+        <option v-if="localModel === null" :value="null" disabled hidden>
+          {{ placeholder }}
+        </option>
+        <slot />
       </select>
     </div>
   </div>
@@ -18,31 +20,37 @@
 <script>
 export default {
   props: {
-    vmodel: { type: String, default: "" },
+    modelValue: { type: String },
     label: { type: String, default: "Label" },
     placeholder: { type: String, default: "Placeholder" },
     disabled: { type: String, default: false },
+  },
+  computed: {
+    localModel: {
+      get() {
+        return this.modelValue;
+      },
+      set(newValue) {
+        this.$emit("update:modelValue", newValue);
+      },
+    },
+    isEmpty() {
+      console.log(this.modelValue, "modelValue");
+      return this.modelValue === null ? "is-empty" : "";
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.field {
-  @include field-styles;
-  position: relative;
-}
-
-.label {
-  @include label-field;
-}
-
 .box-select {
+  position: relative;
   width: 100%;
 
   &::after {
     right: 1.125em;
     z-index: 1;
-    border: 3px solid  $color-primary;
+    border: 3px solid $color-primary;
     border-radius: 2px;
     border-right: 0;
     border-top: 0;
@@ -50,7 +58,7 @@ export default {
     display: block;
     height: 0.625em;
     width: 0.625em;
-    margin-top: 0.6rem;
+    margin-top: -0.4rem;
     top: 50%;
     pointer-events: none;
     position: absolute;
@@ -60,6 +68,8 @@ export default {
 }
 
 .select {
+  cursor: pointer;
+
   @include standard-inputs;
   width: 100%;
   position: relative;
@@ -83,10 +93,9 @@ export default {
   &:disabled {
     @include inputs-disabled;
   }
-}
 
-.option {
-  width: 100%;
-  padding: calc(0.5em - 1px) calc(0.75em - 1px);
+  &.is-empty {
+    color: #b5b5b5;
+  }
 }
 </style>
