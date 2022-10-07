d44bc5fd129e5456b208f1351ca1c652355abca3
argierincon <argierincon@gmail.com>
2022-10-07 10:40:58 -0500

    feature: adding modal component

---
+++ src/components/app/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="modal">
+    <p @click="close">test</p>
+    <slot></slot>
+  </section>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["close"]);
+
+const close = () => emit("close");
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  z-index: 2;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: white;
+}
+</style>
