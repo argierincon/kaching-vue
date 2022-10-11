3e4803fc3607a2c3a0b537a50320812bed3d6295
argierincon <argierincon@gmail.com>
2022-10-11 17:14:29 -0500

    refactor: updating layout
    adding mixin nav menu
    adding modal addincome and add outcome components
    updating padding

--- src/components/layouts/Default.vue
+++ src/components/layouts/Default.vue
@@ -4,16 +4,43 @@
     <main>
       <slot name="main-content"></slot>
     </main>
-    <slot name="nav-menu"></slot>
+    <MoblieModal
+      :showModal="showModalIncome"
+      @close="closeModalIncome"
+      closeOutside
+    >
+      <AddIncome :btnCancel="() => closeModalIncome()" />
+    </MoblieModal>
+    <MoblieModal
+      :showModal="showModalOutcome"
+      @close="closeModalOutcome"
+      closeOutside
+    >
+      <AddOutcome :btnCancel="() => closeModalOutcome()" />
+    </MoblieModal>
+    <NavMenu :optionsMenu="optMenu" />
   </section>
 </template>
 
+<script>
+import NavMenu from "@/components/app/navMenu/Index.vue";
+import menuElemsMixin from "@/mixins/navMenuMixin.js";
+
+export default {
+  name: "navMenu",
+  mixins: [menuElemsMixin],
+  components: { NavMenu },
+};
+</script>
+
 <style lang="scss" scoped>
 .default-layout {
+  width: 100%;
   padding-bottom: 60px;
+  position: absolute;
 
   main {
-    padding: 1.5rem;
+    padding: 2rem 1.5rem;
   }
 }
 </style>
