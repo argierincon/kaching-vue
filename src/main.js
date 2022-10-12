a5f2b0148a1a908ee1a8f9ddd3325933dd6059fb
argierincon <argierincon@gmail.com>
2022-10-12 16:34:47 -0500

    feature: initializing firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+import { initializeApp } from "firebase/app";
 import App from "./App.vue";
 import router from "./router";
+import firebaseConfig from "./firebase";
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 
