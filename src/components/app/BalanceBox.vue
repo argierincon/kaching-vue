ae4fa86eea483be7dd340caed1e058604cd08925
argierincon <argierincon@gmail.com>
2022-10-20 10:56:55 -0500

    feature: adding props

--- src/components/app/BalanceBox.vue
+++ src/components/app/BalanceBox.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="balance-box">
     <p class="balance-box__title">BALANCE</p>
-    <h6 class="balance-box__amount">s/3,500.40</h6>
+    <h6 class="balance-box__amount">{{ currencyFormater.format(balance) }}</h6>
   </div>
 </template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+import { currencyFormater } from "/utils/currencyFormater";
+
+const props = defineProps({
+  balance: { type: Number, default: 0 },
+});
+
+const { balance } = toRefs(props);
+</script>
 
 <style lang="scss" scoped>
 .balance-box {
