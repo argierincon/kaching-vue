4a32dceb808c5fb66d735ba6f17bc6dcc93893b8
argierincon <argierincon@gmail.com>
2022-10-21 08:12:31 -0500

    feature: adding store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { initializeApp } from "firebase/app";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 import firebaseConfig from "./firebase";
 
 initializeApp(firebaseConfig);
@@ -9,5 +10,6 @@ initializeApp(firebaseConfig);
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 app.mount("#app");
