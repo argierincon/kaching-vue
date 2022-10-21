8e1a11ded7ff5adf463e37d03fa3c1f56d93b518
argierincon <argierincon@gmail.com>
2022-10-21 12:00:58 -0500

    feature: getting transacations in history

--- src/components/app/transactionHistory/Index.vue
+++ src/components/app/transactionHistory/Index.vue
@@ -3,6 +3,7 @@
     <template #main-content>
       <h4 class="title">Historial de transacciones</h4>
       <section class="transaction-history">
+        <Loader v-if="isLoading" />
         <TransactionItem
           v-show="!movements.length"
           description="No hay transacciones registradas"
@@ -21,96 +22,19 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import getTransactions from "@/model/getTransactions";
 import Layout from "@/components/layouts/Default.vue";
+import Loader from "@/components/public/Loader.vue";
 import TransactionItem from "@/components/app/transactionHistory/TransactionItem.vue";
-import { reactive } from "vue";
 
-const movements = reactive([
-  {
-    id: 1,
-    date: new Date("10-02-2022"),
-    transactionType: "outcome",
-    transactionName: "Almuerzo parrilla",
-    amount: -49.2,
-  },
-  {
-    id: 2,
-    date: new Date("10-01-2022"),
-    transactionType: "outcome",
-    transactionName: "Almuerzo ramen",
-    amount: -120.0,
-  },
-  {
-    id: 3,
-    date: new Date("09-30-2022"),
-    transactionType: "income",
-    transactionName: "Pago quincena",
-    amount: 2697.64,
-  },
-  {
-    id: 4,
-    date: new Date("09-30-2022"),
-    transactionType: "outcome",
-    transactionName: "Almuerzo chaufa y makis",
-    amount: -44.7,
-  },
-  {
-    id: 5,
-    date: new Date("09-28-2022"),
-    transactionType: "outcome",
-    transactionName: "Mercado del mes Metro",
-    amount: -340.53,
-  },
-  {
-    id: 6,
-    date: new Date("09-28-2022"),
-    transactionType: "outcome",
-    transactionName: "Ahorro en otra cuenta",
-    amount: -508.54,
-  },
-  {
-    id: 7,
-    date: new Date("09-27-2022"),
-    transactionType: "outcome",
-    transactionName: "Retiro de efectivo",
-    amount: -50.0,
-  },
-  {
-    id: 8,
-    date: new Date("09-26-2022"),
-    transactionType: "outcome",
-    transactionName: "Serv. mantenimiento",
-    amount: -160.43,
-  },
-  {
-    id: 9,
-    date: new Date("09-26-2022"),
-    transactionType: "outcome",
-    transactionName: "Gas Calida equivocado",
-    amount: -7.2,
-  },
-  {
-    id: 10,
-    date: new Date("09-26-2022"),
-    transactionType: "outcome",
-    transactionName: "Enel",
-    amount: -31.0,
-  },
-  {
-    id: 11,
-    date: new Date("09-26-2022"),
-    transactionType: "outcome",
-    transactionName: "Cena Rokys",
-    amount: -54.7,
-  },
-  {
-    id: 11,
-    date: new Date("09-26-2022"),
-    transactionType: "outcome",
-    transactionName: "Venezuela",
-    amount: -250.0,
-  },
-]);
+const movements = ref([]);
+let isLoading = ref(true);
+
+onMounted(async () => {
+  movements.value = await getTransactions();
+  isLoading.value = false;
+});
 </script>
 
 <style lang="scss" scoped>
@@ -127,6 +51,7 @@ const movements = reactive([
 }
 
 .transaction-history {
+  position: relative;
   @include laptop {
     height: calc(100vh - 240px);
     max-width: 70%;
