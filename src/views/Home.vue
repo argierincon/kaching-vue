7e8f22f6ba22449331cdf82088f38f3466c15bc5
argierincon <argierincon@gmail.com>
2023-04-11 16:00:50 -0500

    feature: adding values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,17 @@
       <section class="home-main">
         <div class="main-left">
           <div class="home-grid">
-            <BalanceBox class="balance" />
-            <IncomeOutcomeBox class="income" label="Ingresos" />
-            <IncomeOutcomeBox class="outcome" label="Gastos" />
+            <BalanceBox class="balance" :balance="balance" />
+            <IncomeOutcomeBox
+              class="income"
+              label="Ingresos"
+              :amount="income"
+            />
+            <IncomeOutcomeBox
+              class="outcome"
+              label="Gastos"
+              :amount="outcome"
+            />
           </div>
 
           <NavigationTabs class="nav-tabs" :tabs="tabs" />
@@ -30,6 +38,15 @@ import IncomeOutcomeBox from "@/components/app/IncomeOutcomeBox.vue";
 import NavigationTabs from "@/components/app/NavigationTabs.vue";
 import SavingsChart from "@/components/app/homeChart/Index.vue";
 import RecentTransactionsSection from "@/components/app/recentTransactions/Index.vue";
+import {
+  collection,
+  getFirestore,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 export default {
   components: {
@@ -54,7 +71,69 @@ export default {
           component: RecentTransactionsSection,
         },
       ],
+      auth: null,
+      transactions: [],
+      balance: null,
+      outcome: null,
+      income: null,
     };
+  },
+  async created() {
+    await this.getTransactions();
+  },
+  methods: {
+    async getTransactions() {
+      this.auth = getAuth();
+      const uid = localStorage.getItem("uid");
+      const db = getFirestore();
+      const q = query(
+        collection(db, "transactions"),
+        where("uid", "==", uid),
+        orderBy("date", "desc")
+      );
+
+      onSnapshot(q, (querySnapshot) => {
+        const rawTransactions = [];
+        querySnapshot.forEach((doc) => {
+          rawTransactions.push({ doc_id: doc.id, data: doc.data() });
+        });
+
+        this.transactions = rawTransactions
+          .map((ele) => {
+            return {
+              id: ele.doc_id,
+              date: new Date(ele.data.date),
+              transactionType: ele.data.transaction_type,
+              category: ele.data?.category,
+              type: ele.data.type,
+              transactionName: ele.data.name,
+              amount: ele.data.amount,
+              description: ele.data?.description,
+              holder: ele.data?.holder,
+              uid: ele.data.uid,
+            };
+          })
+          .sort((a, b) => {
+            return b.date.getDate() - a.date.getDate();
+          });
+
+        this.income = this.transactions
+          .filter((ele) => ele.transactionType === "income")
+          .reduce((suma, current) => {
+            suma += current.amount;
+            return suma;
+          }, 0);
+
+        this.outcome = this.transactions
+          .filter((ele) => ele.transactionType === "outcome")
+          .reduce((suma, current) => {
+            suma += current.amount * -1;
+            return suma;
+          }, 0);
+
+        this.balance = this.income - this.outcome;
+      });
+    },
   },
 };
 </script>
