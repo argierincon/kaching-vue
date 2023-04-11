67123cf109b8b9ab32d9c88dac970c54447cf3a7
argierincon <argierincon@gmail.com>
2023-04-11 17:14:53 -0500

    refactor: updating transition

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,8 @@ export default {
 
 /* from es cuando inicia la transicion, active es cuando esta sucediendo la transicion y to es cuando finaliza */
 /* enter = inicio, leave = final */
-.fade-home-enter-from,
+
+/* .fade-home-enter-from,
 .fade-home-leave-to {
   opacity: 0;
   position: absolute;
@@ -48,5 +49,16 @@ export default {
 .fade-home-leave-active,
 .fade-home-enter-active {
   transition: opacity 0.4s ease;
+} */
+
+.fade-home-enter-from,
+.fade-home-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+.fade-home-enter-active,
+.fade-home-enter-to {
+  transition: all 0.3s ease-out;
 }
 </style>
