473188b38db1b6801c7c18924990c44f203787be
argierincon <argierincon@gmail.com>
2023-04-10 19:12:47 -0500

    refactor: converting in script setup

--- src/components/app/homeChart/Index.vue
+++ src/components/app/homeChart/Index.vue
@@ -1,12 +1,15 @@
 <template>
   <section class="chart-section">
     <p class="label-saved">{{ graphicLabel }}</p>
-    <h6 class="amount-saved">{{ currencyFormater.format(showAmount) }}</h6>
+    <h6 class="amount-saved">
+      {{ currencyFormater.format(showAmount) }}
+    </h6>
+    {{ listAmounts }}
     <Graphic :amounts="listAmounts" @pointSelected="selected" />
   </section>
 </template>
 
-<script>
+<script setup>
 import {
   collection,
   getFirestore,
@@ -19,200 +22,116 @@ import { getAuth } from "firebase/auth";
 
 import { currencyFormater } from "/utils/currencyFormater";
 import Graphic from "@/components/app/homeChart/Graphic.vue";
+import { ref, computed } from "vue";
 
-export default {
-  components: { Graphic },
-  data() {
-    return {
-      amount: null,
-      totalAmount: null,
-      currencyFormater,
-      tr: [],
-      transactions: [
-        {
-          id: 1,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Venezuela",
-          amount: -250.0,
-        },
-        {
-          id: 2,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Cena Rokys",
-          amount: -54.7,
-        },
-        {
-          id: 3,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Enel",
-          amount: -31.0,
-        },
-        {
-          id: 4,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Gas Calida equivocado",
-          amount: -7.2,
-        },
-        {
-          id: 5,
-          date: new Date("09-26-2022"),
-          transactionType: "outcome",
-          transactionName: "Serv. mantenimiento",
-          amount: -160.43,
-        },
-        {
-          id: 6,
-          date: new Date("09-27-2022"),
-          transactionType: "outcome",
-          transactionName: "Retiro de efectivo",
-          amount: -50.0,
-        },
-        {
-          id: 7,
-          date: new Date("09-28-2022"),
-          transactionType: "outcome",
-          transactionName: "Ahorro en otra cuenta",
-          amount: -508.54,
-        },
-        {
-          id: 8,
-          date: new Date("09-28-2022"),
-          transactionType: "outcome",
-          transactionName: "Mercado del mes Metro",
-          amount: -340.53,
-        },
-        {
-          id: 9,
-          date: new Date("09-30-2022"),
-          transactionType: "outcome",
-          transactionName: "Almuerzo chaufa y makis",
-          amount: -44.7,
-        },
-        {
-          id: 10,
-          date: new Date("09-30-2022"),
-          transactionType: "income",
-          transactionName: "Pago quincena",
-          amount: 2697.64,
-        },
-        {
-          id: 11,
-          date: new Date("10-01-2022"),
-          transactionType: "outcome",
-          transactionName: "Almuerzo ramen",
-          amount: -120.0,
-        },
-        {
-          id: 12,
-          date: new Date("10-02-2022"),
-          transactionType: "outcome",
-          transactionName: "Almuerzo parrilla",
-          amount: -49.2,
-        },
-      ],
-      graphicLabel: "Disponible a la fecha",
-      auth: null,
-    };
-  },
-  computed: {
-    showAmount() {
-      return this.amount !== null ? this.amount : this.totalAmount;
-    },
-    listAmounts() {
-      const last30Days = this.transactions.filter((m) => {
-        const today = new Date();
-        const lastDays = today.setDate(today.getDate() - 30);
+const graphicLabel = ref("Disponible a la fecha");
+const amount = ref(null);
+const totalAmount = ref(null);
+const listAmounts = ref([]);
+const transactions = ref([]);
+const auth = ref(null);
 
-        return m.date > lastDays;
-      });
+const getTransactions = async () => {
+  auth.value = getAuth();
 
-      return last30Days.map((_, i) => {
-        const lastTransactions = last30Days.slice(0, i + 1);
+  const uid = localStorage.getItem("uid");
+  const db = getFirestore();
+  const q = query(
+    collection(db, "transactions"),
+    where("uid", "==", uid),
+    orderBy("date", "desc")
+  );
 
-        return lastTransactions.reduce((suma, current) => {
-          current.sum = suma + current.amount;
-          return suma + current.amount;
-        }, 0);
-      });
-    },
-    totalAmount() {
-      return this.transactions.reduce((suma, m) => {
-        return suma + m.amount;
-      }, 0);
-    },
-  },
-  async mounted() {
-    this.amount = this.listAmounts[this.listAmounts.length - 1];
-    await this.getTransactions();
-    console.log(this.tr);
-  },
-  methods: {
-    async getTransactions() {
-      this.auth = getAuth();
+  onSnapshot(q, (querySnapshot) => {
+    console.log("ME EJECUTE");
+    const rawTransactions = [];
+    querySnapshot.forEach((doc) => {
+      rawTransactions.push({ doc_id: doc.id, data: doc.data() });
+    });
 
-      const uid = localStorage.getItem("uid");
-      const db = getFirestore();
-      const q = query(
-        collection(db, "transactions"),
-        where("uid", "==", uid),
-        orderBy("date", "desc")
-      );
-
-      onSnapshot(q, (querySnapshot) => {
-        const rawTransactions = [];
-        querySnapshot.forEach((doc) => {
-          rawTransactions.push({ doc_id: doc.id, data: doc.data() });
-        });
-
-        this.tr = rawTransactions.map((ele) => {
-          return {
-            id: ele.doc_id,
-            date: ele.data.date,
-            transactionType: ele.data.transaction_type,
-            category: ele.data?.category,
-            type: ele.data.type,
-            transactionName: ele.data.name,
-            amount: ele.data.amount,
-            description: ele.data?.description,
-            holder: ele.data?.holder,
-            uid: ele.data.uid,
-          };
-        });
-
-        // console.log(this.tr, "FFFF");
-      });
-    },
-    selected(pointSelected) {
-      const label = this.formatingDate(pointSelected);
-      this.graphicLabel = `${label[0].toUpperCase()}${label.substring(1)}`;
-
-      this.amount = pointSelected;
-
-      if (pointSelected === undefined) {
-        this.amount = this.listAmounts[0];
-      }
-    },
-    formatingDate(pointSelected) {
-      const options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
+    transactions.value = rawTransactions.map((ele) => {
+      return {
+        id: ele.doc_id,
+        date: new Date(ele.data.date),
+        transactionType: ele.data.transaction_type,
+        category: ele.data?.category,
+        type: ele.data.type,
+        transactionName: ele.data.name,
+        amount: ele.data.amount,
+        description: ele.data?.description,
+        holder: ele.data?.holder,
+        uid: ele.data.uid,
       };
+    });
 
-      if (pointSelected === undefined) {
-        return this.transactions[0].date.toLocaleDateString("es-MX", options);
-      }
+    totalAmount.value = transactions.value.reduce((suma, m) => {
+      return suma + m.amount;
+    }, 0);
 
-      return this.transactions
-        .find((ele) => ele.sum === pointSelected)
-        .date.toLocaleDateString("es-MX", options);
-    },
-  },
+    // Lista de montos transformada para la gráfica
+    const last30Days = transactions.value.filter((m) => {
+      const today = new Date();
+      const lastDays = today.setDate(today.getDate() - 30);
+
+      console.log(m, "M>DATE");
+
+      console.log(new Date(m.date).getDate(), "getDate");
+
+      return m.date > lastDays;
+    });
+
+    listAmounts.value = last30Days.map((_, i) => {
+      const lastTransactions = last30Days.slice(0, i + 1);
+
+      return lastTransactions.reduce((suma, current) => {
+        current.sum = suma + current.amount;
+        return suma + current.amount;
+      }, 0);
+    });
+
+    amount.value = listAmounts.value.length
+      ? listAmounts.value[listAmounts.value.length - 1]
+      : 0;
+
+    console.log("FIN DE LA EJECUCION");
+  });
+};
+
+await getTransactions();
+
+const showAmount = computed(() => {
+  return amount.value !== null ? amount.value : totalAmount.value;
+});
+
+const selected = (pointSelected) => {
+  const label = formatingDate(pointSelected);
+
+  // console.log(pointSelected);
+  graphicLabel.value = `${label[0].toUpperCase()}${label.substring(1)}`;
+  // console.log(graphicLabel.value, "graphicLabel.value ");
+
+  amount.value = pointSelected;
+  if (amount.value === undefined) amount.value = listAmounts.value[0];
+};
+
+const formatingDate = (pointSelected) => {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  if (pointSelected === undefined) {
+    return transactions.value[0].date.toLocaleDateString("es-MX", options);
+  }
+
+  const a = transactions.value.find((ele) => ele.sum === pointSelected);
+
+  console.log(a, "AAAA");
+  return transactions.value
+    .find((ele) => ele.sum === pointSelected)
+    .date.toLocaleDateString("es-MX", options);
 };
 </script>
 
