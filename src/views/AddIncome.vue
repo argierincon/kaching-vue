0927df84d4518744f97fc00f0ca2c92da547e553
argierincon <argierincon@gmail.com>
2023-04-12 16:15:07 -0500

    feature: adding add transactions views

---
+++ src/views/AddIncome.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="max-w-sections">
+    <AddIncome />
+  </section>
+</template>
+
+<script setup>
+import AddIncome from "../components/app/formAddTransaction/AddIncome.vue";
+</script>
+
+<style>
+@media screen and (min-width: 1024px) {
+  .max-w-sections {
+    max-width: 70%;
+    margin: auto;
+  }
+}
+</style>
---
+++ src/views/AddOutcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="max-w-sections">
+    <AddOutcome />
+  </section>
+</template>
+
+<script setup>
+import AddOutcome from "../components/app/formAddTransaction/AddOutcome.vue";
+</script>
