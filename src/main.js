68082902417a6910c122764c3faf708fecf4ac34
argierincon <argierincon@gmail.com>
2023-04-12 16:11:32 -0500

    refactor: adding dynamic layouts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import firebaseConfig from "./firebase";
+import Default from "./components/layouts/Default.vue";
+import Blank from "./components/layouts/Blank.vue";
 
 initializeApp(firebaseConfig);
 
@@ -11,5 +13,7 @@ const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.component("default-layout", Default);
+app.component("blank-layout", Blank);
 
 app.mount("#app");
